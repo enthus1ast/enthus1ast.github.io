16ac78e6676b9637a3984c7a3d75336c5b6e323e
David Krause <enthus1ast@users.noreply.github.com>
2023-01-26 21:59:05 +0100

    :)

--- gristTextWidget/script.js
+++ gristTextWidget/script.js
@@ -1,5 +1,26 @@
 function displayText() {
-  if (location.search) {
-    document.getElementById("text").innerText = decodeURI(location.search.substring(1))
+  const params = new URLSearchParams(location.search);
+  let text = params.get("text")
+  if (text) {
+    document.getElementById("text").innerText = decodeURI(atob(text))
   }
 }
+
+function encode(str) {
+  return location.href + "?text=" + btoa(encodeURI(str))
+}
+
+
+window.addEventListener('DOMContentLoaded', function() {
+  console.log("bound")
+  document.getElementById("in").addEventListener("input", function (ev) {
+    let encoded = encode(this.value)
+    console.log(encoded)
+
+    document.getElementById("out").value = encoded;
+
+
+    document.getElementById("outlink").innerText = encoded;
+    document.getElementById("outlink").href = encoded;
+  })
+})
